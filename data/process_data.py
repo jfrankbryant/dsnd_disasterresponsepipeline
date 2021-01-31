3c356904f2194ef3bfff18c750d1ffc7c915a4f8
J Bryant <literary.sidekick@gmail.com>
2021-01-30 23:43:21 -0500

    Update scripts with code from Jupyter notebooks

--- data/process_data.py
+++ data/process_data.py
@@ -1,35 +1,66 @@
+# import libraries
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Returns two dataframes: one for messages and the other with the categories."""
+    messages = pd.read_csv(messages_filepath)
+    category_column_names = pd.read_csv(categories_filepath, nrows=1)
+    category_column_names = category_column_names.categories.str.split(";",expand=True)
+    category_column_names = category_column_names.iloc[0].str.rstrip('-01').tolist()
+    categories = pd.read_csv(categories_filepath, sep=';', names= category_column_names)
 
+    return messages, categories
 
-def clean_data(df):
-    pass
+def clean_data(messages_df, categories_df):
+    """Returns a merged, cleaned dataframe."""
+    categories_df[['id','related']] = categories_df.related.str.split(",",expand=True)
+    categories_df.drop(0, inplace=True)
+    categories_df = categories_df.reset_index(drop=True)
+    for column in categories_df:
+    # set each value to be the last character of the string
+        categories_df[column] = categories_df[column].str.lstrip("abcdefghijklmnopqrstuvwxyz_-") #can [-1] be used instead of lstrip? yes but it's less exact and causes other issues
+
+    # convert column from string to interger
+        categories_df[column] = categories_df[column].astype(int)
+    df = messages_df.merge(categories_df, how="outer", on='id')
+    drop_indices = list(df.loc[df['related'] == 2].index)
+    df.drop(drop_indices, inplace=True)
+    df = df.reset_index(drop=True)
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Create a SQLite database and save the cleaned dataframe as a table."""
+    engine = create_engine('sqlite:///categorized_disaster_response_messages.db')
+    df.to_sql('categorized_messages', engine, index=False)
 
 
 def main():
+    """This function was provided by Udacity.
+    It utilizes the functions above to load, clean, and save the data.
+    It was modified based on how the data gets cleaned.
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        messages, categories = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
-        df = clean_data(df)
-        
+        df = clean_data(messages, categories)
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,39 +1,89 @@
+#import libraries
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import sqlite3
+import re
+import pickle
+import nltk
+nltk.download(['punkt', 'wordnet'])
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score, f1_score
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def load_data(database_filepath):
-    pass
+    """ Takes a database filepath.
+    Returns dataframe from a table in the database and a lemmatizer.
+    """
+    engine = create_engine(database_filepath)
+    conn = sqlite3.connect(database_filepath)
+    df = pd.read_sql('SELECT * FROM categorized_messages', conn)
+    lemmatizer = WordNetLemmatizer()
 
 
 def tokenize(text):
-    pass
+    """ Takes text and returns tokens of single words."""
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    tokens = word_tokenize(text)
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+
+    return tokens
 
 
-def build_model():
-    pass
+def build_model(X_train, y_train):
+    """ Returns a fitted pipeline."""
+    pipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer(norm='l1')),
+            ('clf', MultiOutputClassifier(AdaBoostClassifier(random_state=42)))
+            ])
+
+        pipeline.fit(X_train, y_train)
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Prints out the classification report for the predicted Y values
+    from the X test values.
+    """
+    y_pred = pipeline.predict(X_test)
+    ycols = list(y.columns)
+
+    print(classification_report(y_test, y_pred, target_names=ycols))
 
 
 def save_model(model, model_filepath):
-    pass
-
+    """Saves the fitted model above to use elsewhere."""
+    #pickle export info from https://stackabuse.com/scikit-learn-save-and-restore-models/
+    with open(model_filepath, ‘wb’) as file:
+        pickle.dump(model, file)
 
 def main():
+    """This function was provided by Udacity.
+    It utilizes the functions above to loads the clean data,
+    builds the model, and make predictions.
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
